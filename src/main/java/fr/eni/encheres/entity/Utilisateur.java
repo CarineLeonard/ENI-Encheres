5763bdd99140ef40336a08fff40d4dd93d47bfdc
Lopez Guillaume <guillaume.lopez2020@campus-eni.fr>
2020-08-24 14:09:31 +0200

    modification de l'entity Utilisateur pour définir la colonne noUtilisateur comme IDENTITY

--- src/main/java/fr/eni/encheres/entity/Utilisateur.java
+++ src/main/java/fr/eni/encheres/entity/Utilisateur.java
@@ -3,6 +3,7 @@ package fr.eni.encheres.entity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
@@ -14,7 +15,7 @@ import javax.persistence.UniqueConstraint;
 public class Utilisateur {
  
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name = "no_utilisateur", nullable = false)
     private Long noUtilisateur;
  
