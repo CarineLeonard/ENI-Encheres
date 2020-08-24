bf1085d1c1e7a821bef69a2c778ff59bc6d8a3a1
Carine Léonard <carine.leonard2020@campus-eni.fr>
2020-08-24 12:16:06 +0200

    no message

--- src/main/java/fr/eni/encheres/entity/UserRole.java
+++ src/main/java/fr/eni/encheres/entity/UserRole.java
@@ -13,7 +13,7 @@ import javax.persistence.UniqueConstraint;
 @Entity
 @Table(name = "User_Role", //
         uniqueConstraints = { //
-                @UniqueConstraint(name = "USER_ROLE_UK", columnNames = { "User_Id", "Role_Id" }) })
+                @UniqueConstraint(name = "USER_ROLE_UK", columnNames = { "no_utilisateur", "Role_Id" }) })
 public class UserRole {
  
     @Id
