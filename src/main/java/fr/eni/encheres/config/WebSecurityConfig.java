7d3a2529125193a22224ec786954cb2cd2af6929
Lopez Guillaume <guillaume.lopez2020@campus-eni.fr>
2020-08-25 18:45:41 +0200

    Mise en place de la sécurisation rest via annotations (les annotations sont activées dans le fichiers WebSecurityConfig via annotations)

--- src/main/java/fr/eni/encheres/config/WebSecurityConfig.java
+++ src/main/java/fr/eni/encheres/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,7 +18,11 @@ import fr.eni.encheres.service.UserDetailsServiceImpl;
 
 //informe spring que c'est une classe de configuration à executer au démarrage de l'app 
 // configure l'application Spring Security 
-@Configuration														
+@Configuration
+@EnableGlobalMethodSecurity(
+		  prePostEnabled = true, 
+		  securedEnabled = true, 
+		  jsr250Enabled = true)
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
--- src/main/java/fr/eni/encheres/rest/UtilisateurController.java
+++ src/main/java/fr/eni/encheres/rest/UtilisateurController.java
@@ -1,5 +1,6 @@
 package fr.eni.encheres.rest;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,8 @@ import fr.eni.encheres.bo.UtilisateurBO;
 
 @RestController
 public class UtilisateurController {
-	
+
+	@Secured("ROLE_USER")
 	@GetMapping("/userInfo2")
 	public UtilisateurBO getUser(@RequestParam(value = "pseudo", defaultValue= "") String pseudo) {
 		return new UtilisateurBO(pseudo);
