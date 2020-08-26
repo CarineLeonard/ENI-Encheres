01e13c15ff95bd053fac5fa0de735e96550b0225
Carine Léonard <carine.leonard2020@campus-eni.fr>
2020-08-26 10:09:35 +0200

    tests, non fonctionnels mais compile

--- src/test/java/fr/eni/encheres/EniEncheresApplicationTests.java
+++ src/test/java/fr/eni/encheres/EniEncheresApplicationTests.java
@@ -1,15 +1,48 @@
 package fr.eni.encheres;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import fr.eni.encheres.dao.UtilisateurRepository;
+import fr.eni.encheres.entity.Utilisateur;
+
+
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
 class EniEncheresApplicationTests {
 
+	@Autowired
+	private TestEntityManager entityManager; 
+	
+	@Autowired
+	private UtilisateurRepository utilisateurRepository; 
+	
 	@Test
 	void contextLoads() {
-	}
+		}
 
+	@Test 
+	public void whenFindByPseudo_thenReturnPseudo() {
+		
+		// when
+		Utilisateur found = utilisateurRepository.findByPseudo("dbuser") ; 
+		
+		// then
+		assertThat(found.getPseudo()).isEqualTo("dbuser"); 
+		
+			}
+
+	
+	
 	
 	
 	
---
+++ src/test/java/fr/eni/encheres/EniEncheresLiveTest.java
@@ -0,0 +1,25 @@
+package fr.eni.encheres;
+
+import fr.eni.encheres.entity.Utilisateur;
+
+public class EniEncheresLiveTest {
+	/* private static final String API_ROOT = "http://localhost:8080/tests";
+	
+	private Utilisateur createUtilisateurEntity() {
+		Utilisateur utilisateur = new Utilisateur(); 
+		utilisateur.setNoUtilisateur(3L);
+		utilisateur.setPseudo("lili");
+		utilisateur.setNom("test");
+		utilisateur.setPrenom("test");
+		utilisateur.setEmail("test.lili@gmail.com");
+		utilisateur.setTelephone("06.15.00.00.00");
+		utilisateur.setRue("ma rue");
+		utilisateur.setCode_postal("10000");
+		utilisateur.setVille("VILLE");
+		utilisateur.setMotDePasse("123");
+		return utilisateur;
+	}
+	*/ 
+	
+	
+}
