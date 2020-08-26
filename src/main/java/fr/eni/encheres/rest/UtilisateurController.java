690fea56fa0352212eb03bb4cce08324c63c9e7e
Lopez Guillaume <guillaume.lopez2020@campus-eni.fr>
2020-08-26 14:53:20 +0200

    Petits progrès sur Mockito (les tests) mais rien de fonctionnel ...

--- src/main/java/fr/eni/encheres/rest/UtilisateurController.java
+++ src/main/java/fr/eni/encheres/rest/UtilisateurController.java
@@ -1,18 +1,24 @@
 package fr.eni.encheres.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import fr.eni.encheres.bo.UtilisateurBO;
+import fr.eni.encheres.dao.UtilisateurRepository;
+import fr.eni.encheres.entity.Utilisateur;
 
 @RestController
 public class UtilisateurController {
 
+	@Autowired
+	private UtilisateurRepository userRepository;
+
 	@Secured("ROLE_USER")
 	@GetMapping("/userInfo2")
-	public UtilisateurBO getUser(@RequestParam(value = "pseudo", defaultValue= "") String pseudo) {
-		return new UtilisateurBO(pseudo);
+	public Utilisateur getUser(@RequestParam(value = "pseudo", defaultValue= "") String pseudo) {
+		Utilisateur user = this.userRepository.findByPseudo(pseudo);
+		return user;
 	}
 }
--- src/test/java/fr/eni/encheres/EniEncheresApplicationTests.java
+++ src/test/java/fr/eni/encheres/EniEncheresApplicationTests.java
@@ -1,38 +1,16 @@
 package fr.eni.encheres;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.ui.Model;
 
-import fr.eni.encheres.controller.MainController;
-import fr.eni.encheres.dao.UtilisateurRepository;
-
-
-
-@RunWith(SpringRunner.class)
 @SpringBootTest
 class EniEncheresApplicationTests {
 	
-	@Autowired
-	private UtilisateurRepository utilisateurrepo ; 
-	
 	@Test
 	void contextLoads() {
-			
-		
 		
 	}
-
-
-	
-	
-	
-	
 	
 }
