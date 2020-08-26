0af770193a5638269580affaa14c8cd5492f6847
Carine Léonard <carine.leonard2020@campus-eni.fr>
2020-08-26 16:02:48 +0200

    push espace

--- src/main/java/fr/eni/encheres/config/WebSecurityConfig.java
+++ src/main/java/fr/eni/encheres/config/WebSecurityConfig.java
@@ -1,6 +1,6 @@
 package fr.eni.encheres.config;
 
-import javax.sql.DataSource;
+import javax.sql.DataSource; 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
